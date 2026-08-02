9f369fccf9a1543f505fde5ecca8b57db9005409
benheroblaw <benheroblade@gmail.com>
2026-08-02 15:54:46 -0500

    08/02-15:54

--- dev-tools/devTerminal.py
+++ dev-tools/devTerminal.py
@@ -21,13 +21,13 @@ def runCommands(command=''):
     os.system("gnome-terminal -e 'bash -c \" python3 ~/.local/share/prokid/text-porn/ ;bash\"'")
 
   elif command == 'fullpush':
-    print('dev')
+    print('dev\n')
     date = datetime.datetime.now()
     os.system('cd ~/.local/share/prokid/text-porn; git add -A; git commit -m ' + str(date.strftime('%m')) + '/' + str(date.strftime('%d')) + '-' + str(date.strftime('%H')) + ':' + date.strftime('%M') + '; git push origin master')
 
     print(dashes)
 
-    print('release')
+    print('release\n')
     os.system('shopt -s dotglob; cd ~/.local/share/prokid/text-porn; cp --verbose * ~/.local/share/prokid/text-viewer;')
     date = datetime.datetime.now()
     os.system('cd ' + os.path.expanduser('~/.local/share/prokid/text-viewer') + '; git add -A; git commit -m ' + str(date.strftime('%m')) + '/' + str(date.strftime('%d')) + '-' + str(date.strftime('%H')) + ':' + date.strftime('%M') + '; git push origin release')
