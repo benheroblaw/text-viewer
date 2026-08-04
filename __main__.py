701a6fe59fce11a25a114be6d39f7a72a33cfe87
benheroblaw <benheroblade@gmail.com>
2026-08-04 15:22:57 -0500

    08/04-15:22

--- __main__.py
+++ __main__.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 import main, importlib, os
 
+def readfile(file=""):
+  """Reads a file."""
+  with open(file) as f:
+    return f.read()
+def readline(file="", line=1):
+  """Reads a line from a file.
+  Lines start at 1."""
+  line -= 1
+  try:
+    with open(file) as f:
+      data = f.readlines()
+      return data[line].replace("\n", "")
+  except: return "error"
+
 while __name__ == '__main__':
-  os.system('git pull')
+  os.system('printf "%b" "Updating... "; git pull origin ' + readfile('extra/origin'))
   main.main()
   print('\nreloading main...')
   importlib.reload(main)
