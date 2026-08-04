dba525e6786b1ba38bbf52ba36aab5252792ec06
benheroblaw <benheroblade@gmail.com>
2026-08-04 15:18:04 -0500

    08/04-15:18

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-import main, importlib
+import main, importlib, os
 
 while __name__ == '__main__':
+  os.system('git pull')
   main.main()
   print('\nreloading main...')
   importlib.reload(main)
