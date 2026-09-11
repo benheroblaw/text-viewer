b7931ec3c6240be0bf293c8965342e8b0c8a5160
benheroblaw <benheroblade@gmail.com>
2026-09-10 22:18:15 -0500

    09/10-22:18

--- devtools/installer.py
+++ devtools/installer.py
@@ -4,14 +4,15 @@ import os, time
 if os.name == "posix":
   terminalPath = os.path.expanduser("~/.local/share/bhrobla/")
 elif os.name == "nt":
-  while True:
-    input('youre fucked buddy :(')
-    exit()
+  terminalPath = os.path.expanduser("~/AppData/Local/bhrobla/")
 if not os.path.exists(terminalPath):
   os.makedirs(terminalPath)
 os.chdir(terminalPath)
 
 os.system('git clone -b release https://github.com/benheroblaw/text-viewer')
+portable = input("Make portable? [y/n]\n")
+if portable == 'y':
+  os.system('echo portable! > extra/portable')
 print("\nSuccessfully installed!")
 time.sleep(2)
 input("\nPress Enter to continue")
